b5b22ee2fc3e77ff4f72c09d2c50bc9a13a57d12
Syoongy <mastersyong@gmail.com>
2022-01-21 20:02:18 +0800

    complete q6

--- ice2-resource/q6/Q6PartC.java
+++ ice2-resource/q6/Q6PartC.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Q6PartC {
     public static void main(String[] args) {
-    
+
         /* Creates a community garden. */
         CommunityGarden garden = new CommunityGarden("Duke Garden");
 
@@ -25,27 +25,40 @@ public class Q6PartC {
         System.out.println("Expected:N.A.");
         System.out.println("Actual  :" + getFarmerDetails(garden, 10));
         System.out.println();
-        
 
     }
 
-
-
     /*
      * Returns the name & gender of farmer who owns this plot of land.
-     * garden     - the specified community garden, assumed not null.
+     * garden - the specified community garden, assumed not null.
      * plotNumber - the specified plotNumber
      *
      * If the plotNumber is invalid, return "N.A."
      * If the plot is currently vacant (i.e. not owned by any farmer),
-     * return  "available".
+     * return "available".
      * Otherwise, return the farmer's name and gender
-     *    e.g. "Amy[Female]"
-     *    e.g. "Bill[Male]"
+     * e.g. "Amy[Female]"
+     * e.g. "Bill[Male]"
      */
     public static String getFarmerDetails(CommunityGarden garden, int plotNumber) {
         // insert your code here.
-        return ""; // added to make this code compile.
+        String retString = "";
+        Plot gardenPlot = garden.getPlot(plotNumber);
+
+        if (gardenPlot == null) {
+            retString = "N.A";
+        } else {
+            Farmer plotFarmer = gardenPlot.getFarmer();
+
+            if (plotFarmer == null) {
+                retString = "available";
+            } else {
+                retString = String.format("%s[%s]", plotFarmer.getName(),
+                        Character.compare(plotFarmer.getGender(), 'M') == 0 ? "Male" : "Female");
+            }
+
+        }
+        return retString; // added to make this code compile.
 
     }
 
--- ice2-resource/q6/Q6PartD.java
+++ ice2-resource/q6/Q6PartD.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Q6PartD {
     public static void main(String[] args) {
-    
+
         /* Creates a community garden. */
         CommunityGarden garden = new CommunityGarden("Duke Garden");
 
@@ -14,30 +14,56 @@ public class Q6PartD {
         System.out.println("Actual  :" + getPercentageOfLeasedPlotsWithCrop(garden, 'V'));
         System.out.println();
         System.out.println("Test 2: Passed in category F(flower)");
-        System.out.println("Expected:33.333333333333336"); 
+        System.out.println("Expected:33.333333333333336");
         System.out.println("Actual  :" + getPercentageOfLeasedPlotsWithCrop(garden, 'F'));
         System.out.println("Note: output may differ slightly due to floating point error.");
         System.out.println();
     }
 
-   
     /*
-     * garden     - the specified community garden, assumed not null.
-     * category   - the crop's category (F, V or H)
+     * garden - the specified community garden, assumed not null.
+     * category - the crop's category (F, V or H)
      * (F - Flower, V - Vegetable , H - Herb)
      *
      * Returns the percentage of leased plots (i.e.
      * plots that has a farmer) with the specified category of crop planted
      * i.e.
-     *     total leased plots with specified category of crop planted
-     *     ------------------------------------------------------------
-     *                         total leased plots
+     * total leased plots with specified category of crop planted
+     * ------------------------------------------------------------
+     * total leased plots
      *
      * If there is no leased plots, print 0.0.
-     * */
+     */
     public static double getPercentageOfLeasedPlotsWithCrop(CommunityGarden garden, char category) {
         // insert your code here.
-        return 0.0;
-       
+        // We first want to get the number of plots within the garden
+        int numPlots = garden.getTotalPlots();
+        int numLeasedPlots = 0;
+        int numLeasedPlotsWCat = 0;
+        double retDbl = 0.0;
+        Plot tmpPlot;
+
+        // We now want to loop through each plot within the garden and check if they
+        // match our category and are leased
+        for (int i = 1; i <= numPlots; i++) {
+            tmpPlot = garden.getPlot(i);
+
+            // We check if the plot is leased and increment the numLeasedPlots if so
+            if (tmpPlot.getFarmer() != null) {
+                numLeasedPlots++;
+
+                // We check if there is crop on the plot and it matches the category passed in.
+                // We increment numLeasedPlotsWCat if true
+                if (tmpPlot.getCrop() != null && Character.compare(tmpPlot.getCrop().getCategory(), category) == 0) {
+                    numLeasedPlotsWCat++;
+                }
+            }
+        }
+
+        if (numLeasedPlots != 0) {
+            retDbl = (numLeasedPlotsWCat * 100.0 / numLeasedPlots);
+        }
+        return retDbl;
+
     }
 }
