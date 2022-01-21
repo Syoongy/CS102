94cc356e31579d4866da620f431a9bfbc774feec
Syoongy <mastersyong@gmail.com>
2022-01-21 18:38:12 +0800

    complete qn4

--- ice2-resource/RadioActiveTest.java
+++ ice2-resource/RadioActiveTest.java
@@ -1,13 +1,17 @@
 public class RadioActiveTest {
 
     public static double calculateMass(double initialMass, double rateOfDecay, int years) {
-        return 0.0;
+        Substance subObj = new Substance(initialMass, rateOfDecay);
+        for (int i = 0; i < years; i++) {
+            subObj.decayOneYear();
+        }
+        return subObj.getMass();
     }
 
     public static void main(String[] args) {
         /*
-         calculate the final mass of a radioactive substance after 5 years
-         when it’s initial mass is 500(gm) and rate of decay is 10 (%),
+         * calculate the final mass of a radioactive substance after 5 years
+         * when it’s initial mass is 500(gm) and rate of decay is 10 (%),
          */
 
         double result = calculateMass(500, 10, 5);
