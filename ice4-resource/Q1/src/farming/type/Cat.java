cc4753f63b9df1bacf43ad11291bc3c33b1f8fd5
Syoongy <mastersyong@gmail.com>
2022-02-05 15:31:15 +0800

    ice4

---
+++ ice4-resource/Q1/src/farming/type/Cat.java
@@ -0,0 +1,9 @@
+package farming.type;
+
+import farming.type.Animal;
+
+public class Cat implements Animal {
+    public void makeNoise() {
+        System.out.println("meow meow");
+    }
+}
---
+++ ice4-resource/Q1/src/farming/type/Dog.java
@@ -0,0 +1,9 @@
+package farming.type;
+
+import farming.type.Animal;
+
+public class Dog implements Animal {
+    public void makeNoise() {
+        System.out.println("woof woof");
+    }
+}
