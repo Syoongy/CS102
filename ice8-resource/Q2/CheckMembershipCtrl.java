37e591ecb5e12ac4b8f1af491e9cbd19bcadee21
Syoongy <mastersyong@gmail.com>
2022-03-08 14:25:12 +0800

    q2

--- ice8-resource/Q2/CheckMembershipCtrl.java
+++ ice8-resource/Q2/CheckMembershipCtrl.java
@@ -7,5 +7,18 @@ public class CheckMembershipCtrl {
     }
 
     // complete the checkMembershipDuration method here
-     
+    public int checkMembershipDuration(String studentName, String teamName) {
+        int retVal = -1;
+        try {
+            Team retTeam = teamDAO.retrieve(teamName);
+            TMSDate dateFormed = retTeam.getDateFormed();
+            Student retStudent = retTeam.retrieve(studentName);
+            TMSDate studentJoined = retStudent.getDateJoined();
+            retVal = studentJoined.calculateDifference(dateFormed);
+        } catch (Exception e) {
+            return retVal;
+        }
+        return retVal;
+    }
+
 }
--- ice8-resource/Q2/CheckMembershipMenu.java
+++ ice8-resource/Q2/CheckMembershipMenu.java
@@ -22,17 +22,37 @@ public class CheckMembershipMenu {
             display();
             choice = sc.nextInt();
             switch (choice) {
-                case 1 :
+                case 1:
                     checkMembershipDuration();
                     break;
-                case 2 :
+                case 2:
                     System.out.println("bye bye");
                     break;
-                default :
+                default:
                     System.out.println("Enter a choice between 1 to 2");
             }
         } while (choice != 2);
     }
 
     // complete the checkMembershipDuration method here
+    public void checkMembershipDuration() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the student's name > ");
+        String studentName = sc.nextLine();
+        System.out.print("Enter the team's name > ");
+        String teamName = sc.nextLine();
+        int duration = ctrl.checkMembershipDuration(studentName, teamName);
+        switch (duration) {
+            case 0:
+                System.out.println(String.format("%s joined the team when the team is formed.%n", studentName));
+                break;
+            case -1:
+                System.out.println(String.format("%s is not in team %s.%n", studentName, teamName));
+                break;
+            default:
+                System.out.println(String.format("%s joined the team %d days after the team is formed.%n", studentName,
+                        duration));
+                break;
+        }
+    }
 }
