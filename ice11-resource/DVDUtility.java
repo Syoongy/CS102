76b7c45671c92142c1ee6ace35d000a82d4aa05b
Syoongy <mastersyong@gmail.com>
2022-03-29 15:10:48 +0800

    cleanup

--- ice11-resource/DVDUtility.java
+++ ice11-resource/DVDUtility.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class DVDUtility {
     public static DVD getMostExpensiveDVD(DVD[] array) {
+
         if (array == null) {
             return null;
         }
+
         DVD retDVD = null;
         for (DVD dvd : array) {
             if (retDVD == null || retDVD.getPrice() < dvd.getPrice()) {
@@ -15,6 +17,7 @@ public class DVDUtility {
     }
 
     public static List<DVD> getDVDWithGenre(DVD[] array, String genre) {
+
         if (array == null || genre == null) {
             return null;
         }
@@ -69,6 +72,7 @@ public class DVDUtility {
 
     public static List<DVD> getUniqueDVDs(DVD[] array, DVD[] anotherArray) {
 
+        // We return the array that is not null if either of the input arrays are null
         if (array == null) {
             return Arrays.asList(anotherArray);
         }
@@ -77,15 +81,21 @@ public class DVDUtility {
             return Arrays.asList(array);
         }
 
+        // We convert both input arrays into lists
         List<DVD> origList = Arrays.asList(array);
         List<DVD> anotherList = Arrays.asList(anotherArray);
 
+        // We create a cmobined set that acts as the union of all DVDs
+        // This elimnates duplicate title after overriding the hashCode and equals
+        // method in the DVD class
         Set<DVD> combinedSet = new HashSet<>(origList);
         combinedSet.addAll(anotherList);
 
+        // We create a set that acts as the intersect of DVDs between both lists
         Set<DVD> intersectingSet = new HashSet<>(origList);
         intersectingSet.retainAll(anotherList);
 
+        // We then remove the intersect from the union
         combinedSet.removeAll(intersectingSet);
 
         return new ArrayList<>(combinedSet);
